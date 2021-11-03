9a37633a76d73d5a954ee3f969803baefc7c8a6c
Krystian Podemski <podemski.krystian@gmail.com>
2021-11-03 12:18:44 +0100

    translations

---
+++ translations/pl.php
@@ -0,0 +1,7 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{kporderlistcarrier}prestashop>kporderlistcarrier_1595f5e5acd8a74bc0457dc79f0c7406'] = 'Nazwa przewoźnika na liście zamówień';
+$_MODULE['<{kporderlistcarrier}prestashop>kporderlistcarrier_21d1f7be2ad0f08c04ad93ebae1c0347'] = 'Wyświetla nazwę przewoźnika na liście zamówień w back office sklepu';
+$_MODULE['<{kporderlistcarrier}prestashop>kporderlistcarrier_c91e596246bbf8fdff9dae7b349d71d9'] = 'Przewoźnik';
